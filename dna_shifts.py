775d0aa5ae2d3eaa0b77f23ed50b79a4ac0742be
maks <01178297@pw.edu.pl>
2024-03-31 22:37:17 +0200

    left shift

--- dna_shifts.py
+++ dna_shifts.py
@@ -23,7 +23,7 @@ def rsob_nuc(nucl1, nucl2):
     return res
 
 print(rsob_nuc([Nucleotide.A, Nucleotide.A], [Nucleotide.C,Nucleotide.G]))
-    
+
 
 
 # input artificial DNA (list); output - one bit right shift DNA (list)
@@ -45,8 +45,49 @@ def r_shift(dna, k):
     else:
         alfa = [Nucleotide.A]*int((k-1)/2) + dna[:-int((k-1)/2)]
         return rsob(alfa)
-    
+
 print(r_shift([Nucleotide.G, Nucleotide.C, Nucleotide.T, Nucleotide.G, Nucleotide.A, Nucleotide.T],3))
 
 
 
+
+# left shift:
+def l_shift(dna, k):
+    if k % 2 == 0:
+        return dna[int(k/2):] + [Nucleotide.A]*int(k/2)
+    else:
+        alfa = dna[int((k-1)/2):] + [Nucleotide.A]*int((k-1)/2)
+        return lsob(alfa)
+
+def lsob(dna):
+    m = len(dna)
+    B1 = [Nucleotide.C] * m
+    B2 = [Nucleotide.G] * m
+    B3 = dna[1:] + [Nucleotide.A]
+    B4 = [x & y for x, y in zip(B2, B3)]
+    B5 = [x & y for x, y in zip(dna, B1)]
+    return lsob_nuc(B4,B5)
+
+def lsob_nuc(nucl1, nucl2):
+    res = []
+    for i in range(len(nucl1)):
+        res.append(lsob_operation(nucl1[i], nucl2[i]))
+    return res
+
+# it uses the same nucleotide operation table as right shift
+def lsob_operation(l0,l1):
+    if (l0.name,l1.name) in rsob_table:
+        return rsob_table[(l0.name,l1.name)]
+    elif (l1.name,l0.name in rsob_table):
+        return rsob_table[(l1.name,l0.name)]
+    else: return None
+
+
+
+print("right shift check: (should be AATA)")
+print(r_shift([Nucleotide.T, Nucleotide.A, Nucleotide.G, Nucleotide.C],4))
+
+print("left shift check: (should be GCAA)")
+print(l_shift([Nucleotide.T, Nucleotide.A, Nucleotide.G, Nucleotide.C],4))
+
+# for not even k, both shifts also work (checked)
