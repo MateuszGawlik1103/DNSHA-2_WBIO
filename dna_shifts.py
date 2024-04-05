e758ca9e1616eb9a00e2ace20f24972db69b20a5
maks <01178297@pw.edu.pl>
2024-04-05 20:54:58 +0200

    left shift, right rotation, dna2bin and bin2dna

--- dna_shifts.py
+++ dna_shifts.py
@@ -10,7 +10,7 @@ rsob_table = {
 def rsob_operation(l0,l1):
     if (l0.name,l1.name) in rsob_table:
         return rsob_table[(l0.name,l1.name)]
-    elif (l1.name,l0.name in rsob_table):
+    elif (l1.name,l0.name) in rsob_table:
         return rsob_table[(l1.name,l0.name)]
     else: return None
 
@@ -66,21 +66,57 @@ def lsob(dna):
     B3 = dna[1:] + [Nucleotide.A]
     B4 = [x & y for x, y in zip(B2, B3)]
     B5 = [x & y for x, y in zip(dna, B1)]
-    return lsob_nuc(B4,B5)
+    # it uses the same nucleotide operation table as right shift
+    return rsob_nuc(B4, B5)
 
-def lsob_nuc(nucl1, nucl2):
-    res = []
-    for i in range(len(nucl1)):
-        res.append(lsob_operation(nucl1[i], nucl2[i]))
-    return res
 
-# it uses the same nucleotide operation table as right shift
-def lsob_operation(l0,l1):
-    if (l0.name,l1.name) in rsob_table:
-        return rsob_table[(l0.name,l1.name)]
-    elif (l1.name,l0.name in rsob_table):
-        return rsob_table[(l1.name,l0.name)]
-    else: return None
+# right rotation
+def r_rotate(dna, k):
+    m = len(dna)
+    B1 = r_shift(dna, k)
+    B2 = l_shift(dna, (2*m)-k)
+    return nuc_or(B1, B2)
+
+
+# the nucleotide operation to imitate the bitwise OR
+def nuc_or(dna1, dna2):
+    binary1 = nuc_to_binary(dna1)
+    binary2 = nuc_to_binary(dna2)
+    if len(binary1) != len(binary2):
+        raise ValueError("not the same length")
+    result = ""
+    for bit1, bit2 in zip(binary1, binary2):
+        if bit1 == '1' or bit2 == '1':
+            result += '1'
+        else:
+            result += '0'
+    return binary_to_nuc(result)
+
+
+# dna to binary
+def nuc_to_binary(dna):
+    nucleotide_to_binary = {'A': '00', 'C': '01', 'G': '10', 'T': '11'}
+    binary_sequence = ""
+    for nucleotide in dna:
+        if nucleotide.name not in nucleotide_to_binary:
+            raise ValueError("invalid nucleotide: {}")
+        binary_sequence += nucleotide_to_binary[nucleotide.name]
+    return binary_sequence
+
+
+# binary to dna
+def binary_to_nuc(binary_sequence):
+    binary_to_nucleotide = {'00': Nucleotide.A, '01': Nucleotide.C,
+                            '10': Nucleotide.G, '11': Nucleotide.T}
+    if len(binary_sequence) % 2 != 0:
+        raise ValueError("invalid binary sequence length")
+    nucleotide_sequence = []
+    for i in range(0, len(binary_sequence), 2):
+        nucleotide_binary = binary_sequence[i:i+2]
+        if nucleotide_binary not in binary_to_nucleotide:
+            raise ValueError("invalid binary representation")
+        nucleotide_sequence.append(binary_to_nucleotide[nucleotide_binary])
+    return nucleotide_sequence
 
 
 
@@ -90,4 +126,8 @@ print(r_shift([Nucleotide.T, Nucleotide.A, Nucleotide.G, Nucleotide.C],4))
 print("left shift check: (should be GCAA)")
 print(l_shift([Nucleotide.T, Nucleotide.A, Nucleotide.G, Nucleotide.C],4))
 
-# for not even k, both shifts also work (checked)
+print("right rotate check: even k (should be GTA)")
+print(r_rotate([Nucleotide.A, Nucleotide.G, Nucleotide.T],4))
+
+print("right rotate check 2: not even k (should be TAA)")
+print(r_rotate([Nucleotide.A, Nucleotide.C, Nucleotide.G],3))
